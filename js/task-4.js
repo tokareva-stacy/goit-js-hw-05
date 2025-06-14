373fa2a82f2359fdc8339e9195fb098f7e4410bf
Nastia Tokarieva <tokarevanyura@gmail.com>
2025-06-14 14:43:12 +0300

    task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,7 +1,8 @@
-const getTotalBalanceByGender = (users, gender) => {
-  
-}
-
+const getTotalBalanceByGender = (users, gender) =>
+  users
+    .filter(user => user.gender === gender)
+    .reduce((total, user) => total + user.balance, 0);
+    
 const clients = [
 	{
     name: "Moore Hensley",
